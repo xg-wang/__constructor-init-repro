72d5bb0cb03f4e7a4d6f9cd3031e53d9504f33ef
Thomas Wang <wangxgwxg@gmail.com>
2018-12-12 17:54:37 -0800

    repro

--- app/app.js
+++ app/app.js
@@ -2,12 +2,23 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import EmberObject from '@ember/object';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
 });
+
+class C extends EmberObject {
+  constructor() {
+    super();
+    this.text = 'hey'
+  }
+}
+
+const c = C.create();
+console.log(c.text);
 
 loadInitializers(App, config.modulePrefix);
 
